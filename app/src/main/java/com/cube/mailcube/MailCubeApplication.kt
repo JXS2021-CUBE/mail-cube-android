a8b2d9a0e907c7218ae47a08753e8bce6d148d3c
wishJinit <wishjinit@gmail.com>
2021-05-22 18:59:37 +0900

    [feat] Start Koin

---
+++ app/src/main/java/com/cube/mailcube/MailCubeApplication.kt
@@ -0,0 +1,20 @@
+package com.cube.mailcube
+
+import android.app.Application
+import com.cube.mailcube.di.viewModelModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class MailCubeApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startDI()
+    }
+
+    private fun startDI() {
+        startKoin {
+            androidContext(this@MailCubeApplication)
+            modules(listOf(viewModelModule))
+        }
+    }
+}
---
+++ app/src/main/java/com/cube/mailcube/di/ViewModelModule.kt
@@ -0,0 +1,5 @@
+package com.cube.mailcube.di
+
+import org.koin.dsl.module
+
+val viewModelModule = module {}
